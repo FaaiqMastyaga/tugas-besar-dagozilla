dc74f2762e934ca9028b23e5d5ccf29099921e9e
faaiqmastyaga10 <faaiqmastyaga10@gmail.com>
2023-05-28 21:17:14 +0700

    Create the function to set PWM for each direction

---
+++ src/control/src/setPWM.cpp
@@ -0,0 +1,38 @@
+#include "setPWM.hpp"
+
+msg::HardwareCommand PWM;
+
+void moveForward() {
+    PWM.motor1 = 0;
+    PWM.motor2 = 0;
+    PWM.motor3 = 0;
+    PWM.motor4 = 0;
+}
+
+void moveLeft() {
+    PWM.motor1 = 0;
+    PWM.motor2 = 0;
+    PWM.motor3 = 0;
+    PWM.motor4 = 0;
+}
+
+void moveBackward() {
+    PWM.motor1 = 0;
+    PWM.motor2 = 0;
+    PWM.motor3 = 0;
+    PWM.motor4 = 0;
+}
+
+void moveRight() {
+    PWM.motor1 = 0;
+    PWM.motor2 = 0;
+    PWM.motor3 = 0;
+    PWM.motor4 = 0;
+}
+
+void Rotate() {
+    PWM.motor1 = 0;
+    PWM.motor2 = 0;
+    PWM.motor3 = 0;
+    PWM.motor4 = 0;
+}
---
+++ src/control/src/setPWM.hpp
@@ -0,0 +1,12 @@
+#ifndef SETPWM_HPP
+#define SETPWM_HPP
+
+#include "msgs/HardwareCommand.h"
+
+void moveForward();
+void moveLeft();
+void moveBackward();
+void moveRight();
+void Rotate();
+
+#endif
