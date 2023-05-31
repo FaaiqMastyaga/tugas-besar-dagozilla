23b6c47c375ec58a6e7d1cce214e864249e3917d
faaiqmastyaga10 <faaiqmastyaga10@gmail.com>
2023-05-31 21:46:32 +0700

    Edit message type that will be published from input

--- src/control/scripts/input_node.py
+++ src/control/scripts/input_node.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Char
+from std_msgs.msg import String
 
 if __name__ == '__main__' :
     try:
         rospy.init_node("input_node")
-        pub = rospy.Publisher("/control/command/pc", Char, queue_size=1)
+        pub = rospy.Publisher("/control/command/pc", String, queue_size=1)
         loop_rate = rospy.Rate(20)
 
         while not rospy.is_shutdown():
-            commandInput = input("Command: ")
+            commandInput = String(input("Command: "))
             pub.publish(commandInput)
             loop_rate.sleep()
 
