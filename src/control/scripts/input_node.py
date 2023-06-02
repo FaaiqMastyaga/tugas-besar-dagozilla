b51d193b263bb415c1c23a1b73daf2dd4175bff4
Albert <albert260306@gmail.com>
2023-06-02 22:34:25 +0700

    launch file & system interrupt completed

--- src/control/scripts/input_node.py
+++ src/control/scripts/input_node.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 import rospy
+import signal
+import sys
 from std_msgs.msg import String
+
+def sigint_handler(signal,frame):
+    rospy.loginfo("SIGINT success, preparing to exit...")
+    sys.exit(0)
 
 if __name__ == '__main__' :
     try:
         rospy.init_node("input_node")
         pub = rospy.Publisher("/control/command/pc", String, queue_size=1)
         loop_rate = rospy.Rate(20)
+        signal.signal(signal.SIGINT, sigint_handler)
 
         while not rospy.is_shutdown():
             commandInput = String(input("Command: "))
